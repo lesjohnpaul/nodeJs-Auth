61aac725a57f2cb3a9fa0ec50f743ba41f0f2649
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-05-28 20:09:48 +0800

    add files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# nodeJs-Auth
+# GoIT Node Lessons
+
+## SETUP
+
+- npm i
+- Make sure PORT, DB_HOST, SECRET_KEY is connected in .env
+- npm run start:dev
+
+## Module 4 - Lesson 7: Authentication and Authorization
+
+- Authentication with JWT
---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import { router as usersRouter } from "./routes/api/usersRouter.js";
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", usersRouter);
+
+app.use((_req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, _req, res, _next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+export { app };
