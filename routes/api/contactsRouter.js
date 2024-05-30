3ac22c0d25f9c588b7c062f63caeca98266f3c4f
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-05-30 20:27:04 +0800

    cs

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -7,8 +7,8 @@ import { authenticateToken } from "../../middlewares/authenticateToken.js";
 const router = express.Router();
 
 /* GET: // http://localhost:3000/api/contacts */
-router.get("/", authenticateToken, ctrlWrapper(getAllContacts));
-
+// router.get("/", authenticateToken, ctrlWrapper(getAllContacts));
+router.get("/", ctrlWrapper(getAllContacts));
 /* GET: // http://localhost:3000/api/contacts/:contactId */
 router.get("/:contactId", authenticateToken, ctrlWrapper(getContactById));
 
